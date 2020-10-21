8a46b46a578fe388c92fe07f9236514b4f08014a
Elev <alja02003@utb.vaxjo.se>
2020-10-21 08:08:37 +0200

    Resten av filerna

---
+++ Template/Template/Map.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Map
+    {
+        private Texture2D house;
+        private int[,] map;
+        private int x;
+        private int y;
+
+        public Map(int width, int height, Texture2D skin)
+        {
+            house = skin;
+            map = new int[width, height];
+            x = width;
+            y = height;
+        }
+
+        public int[,] Get
+        {
+            get { return map; }
+        }
+
+        public void Creat()
+        {
+            for (int i = 5; i>=0; i--)
+            {
+                for(int o = 20; o >= 0; o--)
+                {
+                    map[o, i] = 0;
+                }
+            }
+
+            for (int i = 10; i >= 0; i--)
+            {
+                for (int o = 20; o >= 0; o--)
+                {
+                    map[10 + o,10 + i] = 1;
+                }
+            }
+
+            map[10, 10] = 1;
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+             
+             for (int i = y - 1; i >= 0; i--)
+             {
+                 for (int o = x - 1; o >= 0; o--)
+                 {
+                     if (map[i, o] == 1)
+                     {
+                         spritebatch.Draw(house, new Rectangle(o * 10, i * 10, o * 2, i * 2), Color.Black);
+                     }
+                 }
+             }
+        }
+    }
+}
---
+++ Template/Template/Player.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Player
+    {
+        private Texture2D skin;
+        private Vector2 pos;
+        private int direction;
+        private bool block = false;
+
+        public Player(Texture2D s, Vector2 p)
+        {
+            skin = s;
+            pos = p;
+        }
+
+        /*
+         * b = 10
+         * * * * * * *
+         * * 0 0 * * *
+         * * 0 0 1 * *
+         * * * * 1 * *
+         * * * * * * *
+         * * * * * * *
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+        public void Block(int[,] map)
+        {
+            if (direction == 0)
+            {
+                for(int i = -1; i < 2; i++)
+                {
+                    if (map[(int)pos.X / 10 + 1, (int)pos.Y / 10 + i] == 1)
+                    {
+                        block = true;
+                        pos.X--;
+                    }
+                }
+            }
+            else if (direction == 1)
+            {
+                for (int i = -1; i < 2; i++)
+                {
+                    if (map[(int)pos.X / 10 - 1, (int)pos.Y / 10 + i] == 1)
+                    {
+                        Console.WriteLine((int)pos.Y / 10);
+                        block = true;
+                        pos.X++;
+                    }
+                }
+            }
+            else if (direction == 2)
+            {
+                for (int i = -1; i < 2; i++)
+                {
+                    if (map[(int)pos.X / 10 + i, (int)pos.Y / 10 - 1] == 1)
+                    {
+                        Console.WriteLine((int)pos.Y / 10);
+                        block = true;
+                        pos.Y++;
+                    }
+                }
+
+            }
+            else if (direction == 3)
+            {
+                for (int i = -1; i < 2; i++)
+                {
+                    if (map[(int)pos.X / 10 - i, (int)pos.Y / 10 + 1] == 1)
+                    {
+                        Console.WriteLine((int)pos.Y / 10);
+                        block = true;
+                        pos.Y--;
+                    }
+                }
+
+            }
+            else { block = false; }
+        }
+
+        public void Update()
+        {
+            Console.WriteLine(pos.X + " " + pos.Y);
+            Console.WriteLine(block);
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Right))
+            {
+                if (!block)
+                {
+                    if (pos.X < 790)
+                    {
+                        pos.X += 1;
+                        direction = 0;
+                    }
+                }
+            }
+
+            if (kstate.IsKeyDown(Keys.Left))
+            {
+                if (!block)
+                {
+                    if (pos.X > 0)
+                    {
+                        pos.X -= 1;
+                        direction = 1;
+                    }
+                }
+            }
+            if (kstate.IsKeyDown(Keys.Up))
+            {
+                if (!block)
+                {
+                    if (pos.Y > 0)
+                    {
+                        pos.Y -= 1;
+                        direction = 2;
+                    }
+                }
+            }
+            if (kstate.IsKeyDown(Keys.Down))
+            {
+                if (!block)
+                {
+                    if (pos.Y < 470)
+                    {
+                        pos.Y += 1;
+                        direction = 3;
+                    }
+                }
+            }
+            block = false;
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            Rectangle rec = new Rectangle(pos.ToPoint(), new Point(10, 10));
+            spritebatch.Draw(skin, rec, Color.White);
+        }
+    }
+}
